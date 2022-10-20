690cdf6b73806e6f8f0523c47d2efdd225b08d88
Doyleak <116232888+Doyleak@users.noreply.github.com>
2022-10-19 23:07:02 -0400

    Update Robot_Code_v1.ino

--- Robot_Code_v1.ino
+++ Robot_Code_v1.ino
@@ -1,5 +1,3 @@
-
-
 #include <Bump_Switch.h>
 #include <Encoder.h>
 #include <GP2Y0A21_Sensor.h>
@@ -7,25 +5,20 @@
 #include <Romi_Motor_Power.h>
 #include <RSLK_Pins.h>
 #include <SimpleRSLK.h>
-
 #include "PS2X_lib.h"  //for v1.6
 #include <Servo.h>
 
 /*
-
    PS2 Controller example from the PS2_Lib
    2021-10-08: Refactored for MSP432P401R ZJE
                -Changed pins to good defaults for use with RSLK
                -changed all delay() to delayMicroseconds
-
    Original code: https://github.com/madsci1016/Arduino-PS2X
    Documentation: http://www.billporter.info/2010/06/05/playstation-2-controller-arduino-library-v1-0/
 
    If you are using the PS2 bluetooth dongle, ensure that your
    controller turned on before the program starts
-
 */
-
 
 /******************************************************************
    set pins connected to PS2 controller:
@@ -73,20 +66,15 @@ int STATE = IDLE;
 #define MS 1000
 
 void setup() {
-
   Serial.begin(57600); //ZJE: changed from Arduino deafult of 9600
   gripper.attach(SRV_0);
   gripper.write(5);
   Serial.println("angle = 5");
   delayMicroseconds(500 * 1000); //added delay to give wireless ps2 module some time to startup, before configuring it
-
-  //CHANGES for v1.6 HERE!!! **************PAY ATTENTION*************
-
   //setup pins and settings: GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
   error = 1;
   while (error) {
     error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
-
     if (error == 0) {
       Serial.print("Found Controller, configured successful ");
       Serial.print("pressures = ");
@@ -104,18 +92,14 @@ void setup() {
       Serial.println("Note: Go to www.billporter.info for updates and to report bugs.");
     }  else if (error == 1)
       Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
-
     else if (error == 2)
       Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit www.billporter.info for troubleshooting tips");
-
     else if (error == 3)
       Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
     delayMicroseconds(1000 * 1000);
   }
 
-
   //  Serial.print(ps2x.Analog(1), HEX);
-
   type = ps2x.readType();
   switch (type) {
     case 0:
@@ -135,39 +119,32 @@ void setup() {
 
 void loop() {
   detect();
-
   switch (STATE) {
     case IDLE:
       IdleState();
       break;
-
     case CLOSE:
       CloseState();
       break;
-
     case OPEN:
       OpenState();
       break;
-
     default:
       break;
   }
-
 }
 
 void CloseState() {
-    gripper.write(30);
-    Serial.println("angle = 30");
-    delayMicroseconds(1000 * MS); //can’t use delay
-  
+  gripper.write(30);
+  Serial.println("angle = 30");
+  delayMicroseconds(1000 * MS); //can’t use delay
   STATE = IDLE;
-
 }
+
 void OpenState() {
     gripper.write(160);
     Serial.println("angle = 160");
     delayMicroseconds(1000 * MS); //can’t use delay
-  
   if (ps2x.ButtonPressed(PSB_TRIANGLE)) {
     STATE = CLOSE;
   }
@@ -175,6 +152,7 @@ void OpenState() {
     STATE = OPEN;
   }
 }
+
 void IdleState() {
   if (ps2x.ButtonPressed(PSB_SQUARE))  {           //will be TRUE if button was JUST released
     STATE = OPEN;
@@ -188,15 +166,14 @@ void IdleState() {
    if you don't enable the rumble, use ps2x.read_gamepad(); with no values
    You should call this at least once a second
 */
+
 void detect() {
   if (error == 1) { //skip loop if no controller found
     return;
   }
-
   if (type != 2) { //DualShock or Wireless DualShock Controller
     ps2x.read_gamepad(false, vibrate); //read controller and set large motor to spin at 'vibrate' speed
   }
-
   if (ps2x.Button(PSB_PAD_UP)) {
     Serial.print("Up held this hard: ");
     Serial.println(ps2x.Analog(PSAB_PAD_UP), DEC);
@@ -213,20 +190,16 @@ void detect() {
     Serial.print("DOWN held this hard: ");
     Serial.println(ps2x.Analog(PSAB_PAD_DOWN), DEC);
   }
-
   vibrate = ps2x.Analog(PSAB_CROSS);  //this will set the large motor vibrate speed based on how hard you press the blue (X) button
   if (ps2x.NewButtonState()) {        //will be TRUE if any button changes state (on to off, or off to on)
     if (ps2x.ButtonPressed(PSB_TRIANGLE))
       Serial.println("Triangle pressed");
   }
-
   if (ps2x.ButtonPressed(PSB_CIRCLE))              //will be TRUE if button was JUST pressed
     Serial.println("Circle just pressed");
   if (ps2x.ButtonPressed(PSB_CROSS))              //will be TRUE if button was JUST pressed OR released
     Serial.println("X just changed");
   if (ps2x.ButtonPressed(PSB_SQUARE))             //will be TRUE if button was JUST released
     Serial.println("Square just released");
-
-
   delayMicroseconds(50 * 1000);
 }
